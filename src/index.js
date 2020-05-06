27ddcb6fca6ba241d3253ea44330e65df3a4994b
Suphawich <suphawich.skv@gmail.com>
2020-05-06 12:49:22 +0700

    Initial router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import "dotenv/config"
 import cors from "cors"
 import express from "express"
+import router from "./router"
 
 const app = express()
 
+// Middleware
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method")
-})
+// Router
+app.use("/", router)
 
+// Start Server
 const port = process.env.PORT || 8000
 let app_url = process.env.APP_URL || "http://localhost"
 if (port !== "80") app_url += ":" + port
-
 app.listen(port, () => console.log("Server listening on: " + app_url))
