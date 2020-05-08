1829ec950471da52b3ee7147652de3566539371b
Suphawich <suphawich.skv@gmail.com>
2020-05-08 20:13:34 +0700

    Refactor Vote repo.

--- src/app/repositories/VoteRepository.js
+++ src/app/repositories/VoteRepository.js
@@ -6,38 +6,79 @@ export const imageTextDetection = async () => {
   const imagePath = storage_path(
     "tmp/uploads/capture-1588766049022-762403758.jpg"
   )
-  const imageTextDetectionObj = await GCS.imageTextDetection(imagePath)
+
+  const resultOI = await visionOriginalImage(imagePath)
+
+  const { outputPath } = await createCroppedImage(imagePath, resultOI)
+
+  const { voteAnno } = await visionCroppedImage(outputPath)
+
+  return { voteAnno }
+}
+
+/**
+ * Send Original image to VISION API
+ * @param {String} imagePath
+ */
+const visionOriginalImage = async (imagePath) => {
+  // Send to VISON API
+  const result = await GCS.imageTextDetection(imagePath)
+
   // Get data from VISION response
-  const { numberAnno, width } = GCS.parseData(imageTextDetectionObj)
-  if (numberAnno === undefined) return
+  return GCS.parseData(result)
+}
+
+/**
+ * Send Cropped image to VISION API
+ * @param {String} imagePath
+ */
+const visionCroppedImage = async (imagePath) => {
+  // Send to VISON API
+  const result = await GCS.imageTextDetection(imagePath)
+
+  // Remove cropped image
+  FileHelper.remove(imagePath)
+
+  // Get data from VISION response
+  return GCS.parseDataCrop(result)
+}
+
+/**
+ * Create new image that has cropped
+ * @param {String} imagePath
+ * @param {Annotation} numberAnno
+ */
+const createCroppedImage = async (imagePath, { numberAnno, width }) => {
+  // Validate
+  if (numberAnno === undefined) return {}
 
   // Prepare cropping image
+  let outputPath = ImageHelper.getOutputImagePath(imagePath)
+  const size = calculateSizeOutputImage(numberAnno, width)
+
+  // Cropping image
+  const status = await ImageHelper.cropImage(size, imagePath, outputPath)
+  if (!status) outputPath = undefined
+
+  return { outputPath }
+}
+
+/**
+ * Calculate Width, Height, Left and top of crop image
+ * @param {Object} numberAnno
+ * @param {Int} width
+ */
+const calculateSizeOutputImage = (numberAnno, width) => {
   // Get bounding poly x,y specific left-top (1) and left-bottom (3)
   //  1 -------- 2
   //  |   word   |
   //  3 -------- 4
   const { 0: bfirst, 3: blast } = numberAnno.boundingPoly.vertices
+
   // Cal. height, top of cropped image
-  const cropImageHeight = 150 + blast.y - bfirst.y
+  const height = 150 + blast.y - bfirst.y
   const top = bfirst.y - 100
-  const outputImage = ImageHelper.getOutputImagePath(imagePath)
-  const cropStatus = await ImageHelper.cropImage(
-    {
-      width: width,
-      height: cropImageHeight,
-      left: 0,
-      top: top,
-    },
-    imagePath,
-    outputImage
-  )
-  if (!cropStatus) return
+  const left = 0
 
-  const cropTextDetectionObj = await GCS.imageTextDetection(outputImage)
-  FileHelper.remove(outputImage)
-  // Get data from VISION response
-  const { voteAnno } = GCS.parseDataCrop(cropTextDetectionObj)
-  if (voteAnno === undefined) return
-
-  return { voteAnno: voteAnno }
+  return { width, height, left, top }
 }
